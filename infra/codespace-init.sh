e40cbc0b41031c1946efd87bcd706ac5f4e27393
Cort Lunke <lunk0002@stthomas.edu>
2026-09-21 20:11:37 +0000

    Updated installation directory for AWS CLI

--- infra/codespace-init.sh
+++ infra/codespace-init.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt install -y uuid-runtime
 # CONFIGURE AWS CLI                        #
 ############################################
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/codespace/awscliv2.zip"
-unzip /home/codespace/awscliv2.zip
+unzip /home/codespace/awscliv2.zip -d /home/codespace
 sudo /home/codespace/aws/install
 
 aws configure
